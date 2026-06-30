857fe2f7c28c814e965d5bf6057f45741ecd2874
Beatriz1516 <gomes.pessoa@escola.pr.gov.br>
2026-06-30 09:50:46 -0300

    -Adicionar  a filtragem

--- Pessoas.py
+++ Pessoas.py
@@ -7,6 +7,7 @@ dados = {
     "Nota": [8.5, 7.0, 9.0, 6.5, 8.0]
 }
 
+#EXEMPLO
 mauricio = {
     "nome": "Mauricio",
     "cabelo": "cacheado",
@@ -17,13 +18,40 @@ mauricio = {
     "capital": "R$ 0,00"
 }
 
+#TRANSFORMANDO A LISTA EM UM DATAFRAME DO PANDAS
 df = pd.DataFrame(dados)
+print("=== DATAFRAME ===")
 print(df)
 
-#nomes das pessoas
-pessoas = df['Nome']
-print(f"\n {pessoas}")
 
-#idades
+#ESTATÍSTICAS DAS NOTAS
+print("\n\n=== ESTATÍSTICAS ===")
+print("A média das notas", df['Nota'].mean())
+print("A nota máxima", df['Nota'].max())
+print("A nota mínima", df['Nota'].min())
+print("A soma de todas as notas", df['Nota'].sum())
+
+
+print("\n\n=== DESCRIBE ===")
+print(df.describe())
+
+#FILTRAGEM
+print("\n\n=== NOTAS SUPERIORES QUE 8 ===")
+print(df[df['Nota'] > 8])
+
+print("\n\n=== FREQUÊNCIA DAS CIDADES ===")
+print(df['Cidade'].value_counts())
+
+print("\n\n=== DADOS FALTANTES ===")
+print(df.isnull())
+print(df.isnull().sum())
+
+
+print("\n\n=== NOMES DAS PESSOAS ===")
+pessoas = df['Nome']
+print(f"{pessoas}")
+
+
+print("\n\n=== IDADES ===")
 idades = df['Idade']
-print(f"\n{idades}")
+print(f"{idades}")
